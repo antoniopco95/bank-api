a078ecb0ffd6959a59ba8a268c03a35991a1d38d
João Victor Fagundes <jvfagundes329@gmail.com>
2023-06-21 19:32:26 -0300

    First commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 const app = require('./servidor');
+const routes = require('./routes');
+
+app.use(routes);
 
 app.listen(3000);
---
+++ src/routes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const routes = express();
+const dataBank = require('./bancodedados');
+const users = require('./controllers/users');
+
+
+routes.
+
+    module.export = routes;
