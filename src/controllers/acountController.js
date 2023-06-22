62e15de0aa6cb570400ab652ce92e6e759cd3f89
João Victor Fagundes <jvfagundes329@gmail.com>
2023-06-22 15:14:42 -0300

    Listagem de contas feita

--- src/controllers/acountController.js
+++ src/controllers/acountController.js
@@ -1,13 +1,13 @@
 const dataBank = require('../bancodedados');
 
-exports.listAccounts = (req, res) => {
-    const { senha_banco } = req.params;
+exports.listAcounts = (req, res) => {
+    const bankPassword = req.query.senha_banco;
 
-    if (!senha_banco) {
+    if (!bankPassword) {
         return res.status(400).json({ error: 'Senha do banco não foi informada' });
     }
 
-    else if (senha_banco !== dataBank.banco.senha) {
+    else if (bankPassword !== dataBank.banco.senha) {
         return res.status(401).json({ error: 'Senha do banco incorreta' });
     }
 
--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const accountController = require('./controllers/acountController');
+const acountController = require('./controllers/acountController');
 
-router.get('/contas?senha_banco', accountController.listAccounts);
+router.get('/contas', acountController.listAcounts);
 
 module.exports = router;
