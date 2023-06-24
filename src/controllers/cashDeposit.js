e2a5ba4dd3efcbc35bc4597bbb04a2c5dc53949d
João Victor Fagundes <jvfagundes329@gmail.com>
2023-06-24 10:34:56 -0300

    deposito de conta finalizado

---
+++ src/controllers/cashDeposit.js
@@ -0,0 +1,31 @@
+const dataBank = require('../bancodedados');
+
+exports.depositAmount = (req, res) => {
+    const { numero, valor } = req.body;
+
+    if (!numero || !valor) {
+        return res.status(400).json({ error: 'O numero da conta e o valor do deposito são obrigatorios para prosseguir' });
+    }
+
+    const acount = dataBank.contas.find((conta) => conta.numero === numero);
+
+    if (!acount) {
+        return res.status(400).json({ error: 'Conta inexistente' })
+    }
+
+    if (valor <= 0) {
+        return res.status(400).json({ error: 'O valor depositado deve ser maior que zero' })
+    }
+
+    acount.saldo += valor;
+
+    const depositTransaction = {
+        numero,
+        valor,
+        data: new Date().toISOString()
+    };
+
+    dataBank.depositos.push(depositTransaction);
+
+    res.status(200).json({ message: 'Depósito relizado com sucesso' });
+};
--- src/routes.js
+++ src/routes.js
@@ -3,11 +3,14 @@ const router = express.Router();
 const acountController = require('./controllers/acountController');
 const creatAcount = require('./controllers/createAcount');
 const acountUpdate = require('./controllers/acountUpdate');
-const acountDelete = require('./controllers/acountDelete')
+const acountDelete = require('./controllers/acountDelete');
+const cashDeposit = require('./controllers/cashDeposit');
 
 router.get('/contas', acountController.listAcounts);
 router.post('/contas', creatAcount.createAcounts);
 router.put(`/contas/:numero/usuario`, acountUpdate.acountUpdates);
 router.delete('/contas/:numero', acountDelete.deleteAcount);
+router.post('/transacoes/depositar', cashDeposit.depositAmount);
+
 
 module.exports = router;
