f34c7e77096790af0f4e6929a296d0882d9cfae9
antoniopco95 <antoniopco95@gmail.com>
2023-07-31 07:49:17 -0300

    ajustes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
-const express = require('express');
-const routes = require('./routes');
-const server = require('./servidor');
-
-server.use('/contas', routes);
-
-
+const express = require("express");
+const routes = require("./routes");
+const server = require("./servidor");
+const port = process.env.PORT || 3001;
+server.use("/contas", routes);
